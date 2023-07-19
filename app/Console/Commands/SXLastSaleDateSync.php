863040e604ac231db44aff12582ea0076715f445
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2023-07-19 14:50:29 -0400

    add cron for last date sync

---
+++ app/Console/Commands/SXLastSaleDateSync.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Core\Account;
+use App\Models\Core\Customer;
+use App\Models\SX\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class SXLastSaleDateSync extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sx:last-sale-date-sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to sync mysql last sale date with sx database. Runs nightly';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $cust_numbers = [];
+
+        $invoiced_orders = Order::select('custno', 'invoicedt')
+                    ->where('cono',10)
+                    ->whereIn('whse',['ann','ceda','farm','livo','utic','wate'])
+                    ->where('invoicedt', now()->format('Y-m-d'))
+                    ->get();
+
+        $account = Account::where('subdomain', 'weingartz')->first();
+
+        foreach($invoiced_orders as $invoiced_order)
+        {
+            $cust_numbers[] = $invoiced_order->custno;
+            $customer = Customer::where('sx_customer_number', $invoiced_order->custno)->where('account_id',$account->id)->first();
+            $customer->update(['last_sale_date' => Carbon::parse($invoiced_order->invoicedt)->format('Y-m-d')]);
+        }
+
+        echo "Synced customers are SX# ".implode(", ",$cust_numbers);
+    }
+}
