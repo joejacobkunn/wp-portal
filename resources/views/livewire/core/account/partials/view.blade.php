bd6a875e1bfac3373ef0dd56e6808746fc69158e
anaskm <anaskmm@gmail.com>
2023-07-27 13:39:51 +0530

    WP-8 - Fixed tab load issue

--- resources/views/livewire/core/account/partials/view.blade.php
+++ resources/views/livewire/core/account/partials/view.blade.php
@@ -5,31 +5,30 @@
         activeTabIndex="activeTab"
     >
         <x-slot:tab_content_general>
-            @include('livewire.core.account.partials.general')
+            @if($activeTab == 'general')
+                @include('livewire.core.account.partials.general')
+            @endif
         </x-slot>
 
-        <x-slot:tab_content_credentials>
-            <livewire:core.account.credentials
-                :account="$account"
-                wire:key="account_cred"
-            />
+        <x-slot:tab_content_credentials 
+            component="core.account.credentials"
+            :account="$account"
+            wire:key="account_cred">
         </x-slot>
 
         @can('locations.view')
-            <x-slot:tab_content_locations>
-                <livewire:core.location.index
-                    :account="$account"
-                    wire:key="location_index"
-                />
+            <x-slot:tab_content_locations 
+                component="core.location.index"
+                :account="$account"
+                wire:key="location_index">
             </x-slot>
         @endcan
 
         @can('modules.view')
-            <x-slot:tab_content_modules>
-                <livewire:core.module.index
-                    :account="$account"
-                    wire:key="module_index"
-                />
+            <x-slot:tab_content_modules 
+                component="core.module.index"
+                :account="$account"
+                wire:key="module_index">
             </x-slot>
         @endcan
 
