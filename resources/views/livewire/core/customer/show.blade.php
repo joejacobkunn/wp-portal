91c60bec879edf3e09fc9a56850cbefa7f73c026
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2023-08-23 11:59:42 -0400

    better sx refresh scrript

--- resources/views/livewire/core/customer/show.blade.php
+++ resources/views/livewire/core/customer/show.blade.php
@@ -74,7 +74,7 @@
                         <center>
                             <div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
                                 <span class="visually-hidden">Loading...</span>
-                            </div> Loading Customer Equipments
+                            </div> Loading Customer Equipment
                         </center>
                         @endif
                     </div>
