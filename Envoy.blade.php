2a22d59c2891841335ea87609bda1cc44e4fafd1
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2023-09-06 16:16:11 -0400

    commit envoy file

---
+++ Envoy.blade.php
@@ -0,0 +1,85 @@
+@servers(['staging' => '192.168.1.167', 'production' => '192.168.1.168'])
+
+//setup paths and git
+
+@setup
+$repository = 'git@github.com:joejacobkunn/wp-portal.git';
+$release = date('YmdHis');
+$environment = isset($env) ? $env : "staging";
+$app_dir = ($environment == 'production') ? '/var/www/production/wp-portal' : '/var/www/staging/wp-portal';
+$confirm = ($environment == 'production') ? true : false;
+@endsetup
+
+//define deploy script here in the story
+
+@story('deploy', ['on' => $environment,'confirm' => $confirm])
+git
+composer
+artisan
+queue
+live
+@endstory
+
+//define each part of the story below
+
+//task : GIT (pull from git branch)
+
+@task('git')
+cd {{$app_dir}}
+
+@if($branch)
+echo 'Pulling branch '.$branch;
+git pull origin {{$branch}}
+@elseif($env == 'production')
+echo 'Pulling branch main';
+git pull origin main
+@else
+echo 'Pulling branch dev';
+git pull origin dev
+@endif
+
+@endtask
+
+//task : COMPOSER (run composer tasks)
+
+@task('composer')
+echo 'Running composer...';
+cd {{$app_dir}}
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+composer dump-autoload
+sudo chown -R www-data:www-data storage
+@endtask
+
+//task : artisan (run laravel tasks)
+
+@task('artisan')
+echo 'Running artisan commands...';
+cd {{$app_dir}}
+php artisan down
+
+php artisan cache:clear
+php artisan route:cache
+php artisan view:cache
+php artisan config:cache
+
+php artisan migrate --force
+
+php artisan db:seed --class=RolePermissionSeeder
+@if($seed)
+php artisan db:seed --class={{$seed}}
+@endif
+
+@endtask
+
+//task : queue
+
+@task('queue')
+echo 'Restarting queues';
+cd {{$app_dir}}
+php artisan queue:restart --no-interaction
+@endtask
+
+@task('live')
+cd {{$app_dir}}
+php artisan up
+@endtask
