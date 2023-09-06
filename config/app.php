5cccff013fdb77bb4fb0aaa5c173b24d6aba5433
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2023-09-06 09:26:04 -0400

    telescope install

--- config/app.php
+++ config/app.php
@@ -202,6 +202,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         \App\Providers\ComponentServiceProvider::class,
         App\Providers\ProgressServiceProvider::class,
 
