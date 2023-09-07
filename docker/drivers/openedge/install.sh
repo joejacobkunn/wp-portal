a626fc725c1976b741d4c2f869bed5297d0696f2
anaskm <anaskmm@gmail.com>
2023-09-07 16:50:21 +0530

    Setup odbc driver path in installation script

--- docker/drivers/openedge/install.sh
+++ docker/drivers/openedge/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#Remvoe existing path
+#Remove existing path
 rm -rf /usr/wrk /usr/wrk_oemgmt/ /usr/oemgmt /usr/dlc/
 expect_script="/var/www/html/docker/drivers/openedge/execute.sh"
 expect "$expect_script"
+
+#Setup OpenEdge ODBC driver
+cd /usr/dlc/odbc/lib
+if ldd pgoe27.so | grep "27.so" | grep -q "not found"; then
+    cp libpgicu27.so /usr/lib
+fi
+
+#Set up ODBCINST
+cp /var/www/html/docker/drivers/openedge/src/etc/odbcinst.ini /etc/odbcinst.ini
