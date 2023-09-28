ee81c435abb2f0000367d13e3e9cbbddf7913754
Mohamed Nafil <nafil161@gmail.com>
2023-09-28 13:16:10 +0530

    WP-11 Selectbox code updated

--- resources/views/livewire/component/x-select-field.blade.php
+++ resources/views/livewire/component/x-select-field.blade.php
@@ -1,13 +1,13 @@
 <div>
     <div id="div-{{ $fieldDomId }}" class="x-multiselect">
-        <select id="{{ $fieldDomId }}" {{ $multiple ? 'multiple' : ''}} {{ $disabled ? 'disabled' : ''}}>
+        <select id="{{ $fieldDomId }}" name="{{ $fieldName }}" {{ $multiple ? 'multiple' : ''}} {{ $disabled ? 'disabled' : ''}}>
             @if($selectAllOption)
                 <optgroup label="Select All" class="select-all-item">
             @endif
             @foreach ($items as $item)
-                <option value="{{ $item['value'] }}" 
-                    {{ !empty($item['default']) && !$multiple && empty($selectedItem) ? 'selected' : '' }} 
-                    {{ !empty($item['disabled']) && !$defaultOptionSelectable ? 'disabled' : '' }} 
+                <option value="{{ $item['value'] }}"
+                    {{ !empty($item['default']) && !$multiple && empty($selectedItem) ? 'selected' : '' }}
+                    {{ !empty($item['disabled']) && !$defaultOptionSelectable ? 'disabled' : '' }}
                     {{ in_array($item['value'], $selectedItem) ? 'selected' : '' }}>{{ $item['text'] }}</option>
             @endforeach
             @if($selectAllOption)
@@ -16,7 +16,6 @@
         </select>
     </div>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.27.1/slimselect.min.css" rel="stylesheet"></link>
     <script>
         var isMultiple = false
         @if($multiple)
@@ -24,17 +23,17 @@
         @endif
 
         var selectTimer = selectTimer || null;
-        
+
         if (typeof SlimSelect == 'function') {
             initPlugin()
         } else {
             loadScript("https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.27.1/slimselect.min.js", initPlugin);
         }
-        
+
         function initPlugin() {
             var selectField = new SlimSelect({
                 select: '#{{ $fieldDomId }}',
-                allowDeselect: true,
+                allowDeselect: Boolean('{{ $allowDeselect }}'),
                 deselectLabel: '<i class="fas fa-times"></i>',
                 placeholder: '{{ $placeholder }}',
                 showSearch: Boolean('{{ empty($hideSearch) }}'),
@@ -50,12 +49,26 @@
             })
 
             function setData (values) {
+
+                setTimeout(() => {
+                    if (!document.querySelector('select[name={{ $fieldName }}]').closest('.x-multiselect').querySelector('.input-loading')) {
+                        let loaderDom = document.createElement('i');
+                        loaderDom.classList.add('fa', 'fa-spinner', 'fa-spin', 'input-loading')
+                        document.querySelector('select[name={{ $fieldName }}]').closest('.x-multiselect').querySelector('.ss-main').appendChild(loaderDom)
+                    }
+                }, 200)
+
                 if (selectTimer) {
                     clearTimeout(selectTimer)
                 }
 
                 selectTimer = setTimeout(() => {
-                    window.livewire.emit("{{ $listener }}", '{{ $fieldId }}', values)
+                    document.dispatchEvent(new CustomEvent("browser:{{ $fieldId }}:changed", { "detail": {
+                        value: values
+                    } }));
+
+                    @this.setValue(values)
+                    selectField.data.setSelected(values)
                 }, 600)
             }
 
@@ -71,7 +84,7 @@
                     }, 300)
                 }
             }, true);
-            
+
             initializeListeners();
             function initializeListeners() {
                 let allFieldCount = selectField.data.data[0].options.length
@@ -105,7 +118,29 @@
                 })
             }
 
-            @endif
+                @endif
         }
+    </script>
+
+    <script wire:ignore>
+       (function () {
+        var element = document.querySelector("select[name={{ $fieldName }}]")
+
+        if (!window.listeners) {
+            window.listeners = [];
+        }
+
+        if (!element.getAttribute("emitListener") && !window.listeners.includes('{{ $listener }}' + ':emit')) {
+            window.addEventListener('{{ $listener }}' + ':emit', callbackListener)
+            element.setAttribute("emitListener", true)
+            window.listeners.push('{{ $listener }}' + ':emit')
+        }
+    })()
+
+    function callbackListener(event) {
+        if (document.querySelector('select[name={{ $fieldName }}]') && document.querySelector('select[name={{ $fieldName }}]').closest('.x-multiselect').querySelector('.input-loading')) {
+            document.querySelector('select[name={{ $fieldName }}]').closest('.x-multiselect').querySelector('.input-loading').remove();
+        }
+    }
     </script>
 </div>
