9d0a5c37ba606e4f9f022b9634470f3b20842c53
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-01-09 09:50:34 -0500

    updated product temp script

--- database/seeders/ProductTempSeeder.php
+++ database/seeders/ProductTempSeeder.php
@@ -18,30 +18,11 @@ class ProductTempSeeder extends Seeder
 
        for($i=0; $i<150000;$i++)
        {
-            DB::connection('zxt')->table('pub.productsync')->insert([
-                'cone' => 10,
-                'icsc' => 1,
-                'icsp' => 1,
-                'whse' => 'all',
-                'prod' => 'MX00-'.$i,
-                'vendno' => rand(50000, 60000),
-                'prodcat' => $faker->randomElement(['PART', 'EQUIPMENT', 'ACCESSORIES']),
-                'prodline' => 'CC-P',
-                'descrip' => 'Washer locking ring-type putty color 1/4',
-                'standardpack' => 'EA',
-                'weight' => 1,
-                'length' => 2,
-                'width' => 2,
-                'height' => 4,
-                'listprice' => $faker->randomFloat(2),
-                'baseprice' => $faker->randomFloat(2),
-                'costprice' => $faker->randomFloat(2),
-                'pricecode' => '<5',
-                'operinit' => 'mtm',
-                'transdt' => date('Y/m/d'),
-                'transtm' => '2040',
-                'transproc' => 'pim'
-            ]);
+        $product_name = 'MX-001-'.$i;
+
+        DB::connection('zxt')->statement("INSERT INTO pub.productSync (cono,icsc,icsp,icsw,whse,prod,vendno,prodcat,prodline,descrip,standardpack,weight,LENGTH,width,height,listprice,baseprice,costprice,pricecode,operinit,transdt,transtm,transproc)
+        VALUES ('10',1,1,1,'all','".$product_name."',56500,'PART','CC-P','Washer locking ring-type putty color ID','EA',1,2,2,2,1.25,1.15,0.9,'<5','mtm','01/03/24','2040','pim')");
+
        }
     }
 }
