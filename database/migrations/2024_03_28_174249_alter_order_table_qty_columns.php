cbb2ced769109b4f6c8413aa08a97af33600f933
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-03-28 13:50:08 -0400

    fix qty ship bug

---
+++ database/migrations/2024_03_28_174249_alter_order_table_qty_columns.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('qty_ship')->change();
+            $table->string('qty_ord')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->tinyInteger('qty_ship')->change();
+            $table->tinyInteger('qty_ord')->change();
+
+        });
+    }
+};
