73fddba4d8aab44f88eed3789164246341c898f8
arunRecbrT <arunraj@recybertech.com>
2024-08-30 11:06:56 +0530

    WP-15 settings fix

--- resources/views/livewire/order/partials/settings-table-btn.blade.php
+++ resources/views/livewire/order/partials/settings-table-btn.blade.php
@@ -1,18 +1,20 @@
-<div class="btn-group mb-1 me-2">
-    <div class="dropdown">
-        <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
+<div class="table-action mb-1 me-2">
+    <div class="dropdown-tab text-center" x-data="{ isOpen: false }">
+        <button type="button" class="btn btn-outline-secondary dropdown-toggle"  @click="isOpen = true">
             <i class="fas fa-cog"></i>
         </button>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" wire:navigate href="{{ route('order.email-template.index') }}">Templates</a>
-            <a class="dropdown-item" wire:click="$toggle('enableEcomZwhs')">Show ECOM and ZWHS :
-                @if (!$this->enableEcomZwhs)
-                    <span class="badge bg-light-danger">No</span>
-                @else
-                    <span class="badge bg-light-success">Yes</span>
-                @endif
-            </a>
+        <div x-show="isOpen" @click.away="isOpen = false" class="popup custom-width">
+            <div class="popup-content">
+                <a class="popup-item" wire:navigate href="{{ route('order.email-template.index') }}">Templates</a>
+                <a class="popup-item" @click="isOpen = false" wire:click="$toggle('enableEcomZwhs')">Show ECOM and ZWHS :
+                    @if (!$this->enableEcomZwhs)
+                        <span class="badge bg-light-danger">No</span>
+                    @else
+                        <span class="badge bg-light-success">Yes</span>
+                    @endif
+                </a>
+            </div>
         </div>
     </div>
 </div>
+
