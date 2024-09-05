ce3f5111f2993f30d8cb3df92e1443e4cee2bb91
joejacobkunn <32693537+joejacobkunn@users.noreply.github.com>
2024-09-05 11:22:20 -0400

    Revert "table fixes"

--- app/Http/Livewire/Order/Show.php
+++ app/Http/Livewire/Order/Show.php
@@ -260,8 +260,7 @@ class Show extends Component
 
     public function sendEmail()
     {
-            $this->validate();
-            $this->authorize('manage', $this->order);
+        $this->authorize('manage', $this->order);
             $this->order->status = OrderStatus::FollowUp->value;
             $this->order->last_updated_by = auth()->user()->id;
             $this->order->last_followed_up_at = now();
@@ -272,7 +271,6 @@ class Show extends Component
 
     public function sendShippingEmail()
     {
-        $this->validate();
         $this->authorize('manage', $this->order);
         $this->order->status = OrderStatus::ShipmentFollowUp->value;
         $this->order->last_updated_by = auth()->user()->id;
@@ -285,7 +283,6 @@ class Show extends Component
 
     public function sendReceivingEmail()
     {
-        $this->validate();
         $this->authorize('manage', $this->order);
         $this->order->status = OrderStatus::ReceivingFollowUp->value;
         $this->order->last_updated_by = auth()->user()->id;
