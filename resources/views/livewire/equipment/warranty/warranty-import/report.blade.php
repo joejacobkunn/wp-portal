456499b44b6af7dfb6a2661d77a3a6830aab9a93
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-09-19 14:09:10 -0400

    reporting features

--- resources/views/livewire/equipment/warranty/warranty-import/report.blade.php
+++ resources/views/livewire/equipment/warranty/warranty-import/report.blade.php
@@ -2,8 +2,10 @@
     <div class="card border-light shadow-sm warranty-tab">
         <div class="card-body">
             <div class="alert alert-light-primary color-primary"><i class="fas fa-info-circle"></i> Showing registration
-                date for
-                the last six months in SX</div>
+                data for
+                the last one year from SX. This list is refreshed hourly<span class="float-end"><strong>Last
+                        refreshed</strong>
+                    {{ Carbon\Carbon::parse($last_refresh_timestamp)->diffForHumans() }}</span></div>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                     <livewire:equipment.warranty.warranty-import.report-table lazy
