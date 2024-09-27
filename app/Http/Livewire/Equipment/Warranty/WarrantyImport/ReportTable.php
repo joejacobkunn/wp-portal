2e052e85eb60f190f098d7d2374a9196c753ce70
arunRecbrT <arunraj@recybertech.com>
2024-09-27 15:15:34 +0530

    WP-18 working filter

--- app/Http/Livewire/Equipment/Warranty/WarrantyImport/ReportTable.php
+++ app/Http/Livewire/Equipment/Warranty/WarrantyImport/ReportTable.php
@@ -1,133 +1,156 @@
 <?php
+
 namespace App\Http\Livewire\Equipment\Warranty\WarrantyImport;
 
 use App\Http\Livewire\Component\DataTableComponent;
 use App\Models\Core\Warehouse;
 use App\Models\Equipment\Warranty\Report;
-use Illuminate\Database\Eloquent\Builder;
-use App\Models\Equipment\Warranty\WarrantyImport\WarrantyImports;
 use App\Models\Product\Brand;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
+use Illuminate\Support\Collection;
+use Rappasoft\LaravelLivewireTables\Views\Filters\DateRangeFilter;
 
 class ReportTable extends DataTableComponent
 {
     public $warehouses;
-    public $brand;
-        public function configure(): void
-        {
-            $this->setPrimaryKey('id');
-            //$this->setDefaultSort('warranty_imports.created_at', 'desc');
-            $this->setPerPageAccepted([25, 50, 100]);
-            $this->setTableAttributes([
-                'class' => 'table table-bordered',
-            ]);
-        }
+    public $brands;
+    public array $filterValues = [];
 
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+        $this->setPerPageAccepted([25, 50, 100]);
+        $this->setTableAttributes([
+            'class' => 'table table-bordered',
+        ]);
+
+    }
 
     public function mount()
     {
         $this->warehouses = Warehouse::orderBy('title')->pluck('title', 'short')->toArray();
-        $this->brand = Brand::pluck('name', 'name')->toArray();
+        $this->brands = Brand::orderBy('name')->pluck('name', 'name')->toArray();
     }
 
     public function columns(): array
     {
         return [
             Column::make('Store', 'store')
-            ->secondaryHeader($this->getFilterByKey('whse'))
-
-            ->html(),
+                ->secondaryHeader($this->getFilterByKey('store'))
+                ->format(function ($value, $row) {
+                    return strtoupper($value);
+                })
+                ->sortable()
+                ->searchable(),
 
             Column::make('Customer Number', 'cust_no')
-            ->hideIf(1)
-            ->html(),
+                ->hideIf(1)
+                ->html(),
 
             Column::make('Description', 'description')
-            ->hideIf(1)
-            ->html(),
-
+                ->hideIf(1)
+                ->html(),
 
             Column::make('Customer Name', 'customer_name')
-            ->format(function ($value, $row) {
-                return $value.' ('.$row->cust_no.')';
-            })
-            ->html(),
+                ->format(function ($value, $row) {
+                    return $value.' ('.$row->cust_no.')';
+                })
+                ->sortable()
+                ->searchable(),
+                Column::make('Order Number', 'order_no')
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
+                Column::make('Brand', 'brand')
+                ->secondaryHeader($this->getFilterByKey('brand'))
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Order Number', 'order_no')
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Prod Line', 'prodline')
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Brand', 'brand')
-            ->secondaryHeader($this->getFilterByKey('brand'))
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Model', 'model')
+                ->searchable()
+                ->format(function ($value, $row) {
+                    return $value.' ('.$row->description.')';
+                })
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Prod Line', 'prodline')
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Serial', 'serial')
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Model', 'model')
-            ->searchable()
-            ->format(function ($value, $row) {
-                return $value.' ('.$row->description.')';
-            })
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Sold On', 'sold')
+                ->format(function ($value, $row) {
+                    return date("F j, Y", strtotime($value));
+                })
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Serial', 'serial')
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Reg Date', 'registration_date')
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Sold On', 'sold')
-            ->format(function ($value, $row) {
-                return date("F j, Y", strtotime($value));
-            })
-            ->excludeFromColumnSelect()
-            ->html(),
+                Column::make('Reg By', 'registered_by')
+                ->searchable()
+                ->excludeFromColumnSelect()
+                ->html(),
 
-            Column::make('Reg Date', 'registration_date')
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
-
-            Column::make('Reg By', 'registered_by')
-            ->searchable()
-            ->excludeFromColumnSelect()
-            ->html(),
-
-        ];
+            ];
     }
+
     public function filters(): array
     {
         return [
-            SelectFilter::make('Store', 'whse')
-            ->hiddenFromMenus()
-            ->options([''=>'All']+ $this->warehouses)
-            ->filter(function (Builder $builder, string $value) {
-                $builder->where(DB::raw('lower(store)'), strtolower($value));
-            }),
+            SelectFilter::make('store')
+                ->options([''=>'All Stores']+$this->warehouses)
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->whereRaw('LOWER(store) = ?', [strtolower($value)]);
+                }),
 
-            SelectFilter::make('Brand', 'brand')
+            // Add more filters here as needed
+            SelectFilter::make('brand')
+                ->options(['All Brands'] + $this->brands)
+                ->filter(function(Builder $builder, string $value) {
+                    $builder->where('brand', $value);
+                }),
+
+            DateRangeFilter::make('Sold On', 'sold_on')
             ->hiddenFromMenus()
-            ->options([''=>'All']+ $this->brand)
-            ->filter(function (Builder $builder, string $value) {
-                $builder->where(DB::raw('lower(brand)'), strtolower($value));
-            }),
+                ->config(['placeholder' => 'Enter Date Range'])
+                ->filter(function (Builder $builder, array $dateRange) { // Expects an array.
+                    $builder
+                        ->whereDate('sold', '>=', $dateRange['minDate']) // minDate is the start date selected
+                        ->whereDate('sold', '<=', $dateRange['maxDate']); // maxDate is the end date selected
+                }),
         ];
     }
 
     public function builder(): Builder
     {
         $query = Report::query();
+
+        // Apply filters
+        foreach ($this->getFilters() as $filter) {
+            $filterName = $filter->getName();
+            if (isset($this->filterValues[$filterName]) && $this->filterValues[$filterName] !== '') {
+                $query->where($filterName, $this->filterValues[$filterName]);
+            }
+        }
         return $query;
+    }
+
+    public function updatedFilterValues($value, $key)
+    {
+        $this->resetPage();
     }
 }
