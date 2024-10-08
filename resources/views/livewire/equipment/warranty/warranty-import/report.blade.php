f58037e87e6e088b30ac2cabc0c472af92b240e3
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-10-08 11:22:33 -0400

    report changes

--- resources/views/livewire/equipment/warranty/warranty-import/report.blade.php
+++ resources/views/livewire/equipment/warranty/warranty-import/report.blade.php
@@ -3,12 +3,12 @@
         <div class="card-body">
             <div class="alert alert-light-primary color-primary"><i class="fas fa-info-circle"></i> Showing registration
                 data for
-                the last one year from SX. This list is refreshed hourly<span class="float-end"><strong>Last
+                the last two years from SX. This list is refreshed hourly<span class="float-end"><strong>Last
                         refreshed</strong>
                     {{ Carbon\Carbon::parse($last_refresh_timestamp)->diffForHumans() }}</span></div>
             @if ($non_registered_count > 0)
                 <div class="alert alert-light-warning color-warning"><i class="fas fa-exclamation-triangle"></i>
-                    <strong>{{ $non_registered_count }}</strong> has missing warranty registration(s)
+                    <strong>{{ $non_registered_count }}</strong> products have missing warranty registration(s)
                 </div>
             @endif
             <div class="tab-content" id="myTabContent">
