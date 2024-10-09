b72a153e847062bf376850351e0526050b89d559
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-10-09 14:29:44 -0400

    order page improvements

--- resources/views/livewire/order/partials/result-count.blade.php
+++ resources/views/livewire/order/partials/result-count.blade.php
@@ -1,5 +1,5 @@
 <div class="order-total-count  w-100 d-flex justify-content-between align-items-center ml-2">
     <span class="badge bg-light-primary d-flex align-items-center w-100 order-padding order-margin-left">
-        {{ $this->filteredRowCount }} results
+        {{ number_format($this->filteredRowCount) }} results
     </span>
 </div>
