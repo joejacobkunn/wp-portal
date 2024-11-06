f8b7debe415936f757d6e8bdae6f92f3e1b5e84e
arunRecbrT <arunraj@recybertech.com>
2024-11-07 01:13:34 +0530

    WP-21 warranty report export

--- app/Http/Livewire/Equipment/Warranty/WarrantyImport/Report.php
+++ app/Http/Livewire/Equipment/Warranty/WarrantyImport/Report.php
@@ -2,19 +2,25 @@
 namespace App\Http\Livewire\Equipment\Warranty\WarrantyImport;
 
 use App\Http\Livewire\Component\Component;
+use App\Jobs\ExportWarrantyReport;
 use App\Models\Equipment\Warranty\BrandConfigurator\BrandWarranty;
 use App\Models\Equipment\Warranty\Report as WarrantyReport;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Report extends Component
 {
-    use AuthorizesRequests;
+    use AuthorizesRequests, LivewireAlert;
 
     public $last_refresh_timestamp;
     public $non_registered_count;
 
+    protected $listeners = [
+        'exportWarrantyRecords' => 'exportWarrantyRecords'
+    ];
     public function mount()
     {
         $this->updateBreadcrumb();
@@ -56,5 +62,11 @@ class Report extends Component
         DB::connection('sx')->statement("UPDATE pub.icses SET user9 = '2001-01-01' , user4 = '".auth()->user()->sx_operator_id."' where cono = 10 and currstatus = 's' and LTRIM(RTRIM(UPPER(icses.serialno))) = '".$serial_no."' and custno = '".$cust_no."'");
         $record = WarrantyReport::where('serial',$serial_no)->where('cust_no',$cust_no)->first();
         $record->update(['registration_date' => '2001-01-01', 'registered_by' => auth()->user()->sx_operator_id]);
+    }
+
+    public function exportWarrantyRecords()
+    {
+        ExportWarrantyReport::dispatch(Auth::user());
+        $this->alert('success', 'records will be mailed after export is completed!');
     }
 }
