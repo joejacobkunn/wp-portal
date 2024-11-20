84b8cc45e48f37ee83aed07df875893b6cd64687
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-11-20 13:51:18 -0500

    add listeners to queue

--- app/Events/Orders/OrderCancelled.php
+++ app/Events/Orders/OrderCancelled.php
@@ -8,10 +8,11 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class OrderCancelled
+class OrderCancelled implements ShouldQueue
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
