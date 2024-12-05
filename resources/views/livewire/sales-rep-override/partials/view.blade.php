99a8fb605ca83357dfd481304829a7e358ee565e
Joseph Kunnummyalil <joejacobkunn@gmail.com>
2024-12-05 15:21:01 -0500

    better overridde view page

--- resources/views/livewire/sales-rep-override/partials/view.blade.php
+++ resources/views/livewire/sales-rep-override/partials/view.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
     <div class="col-8 col-md-8 col-xxl-8">
         <div class="card border-light shadow-sm mb-4">
-            <div class="card-header border-gray-300 p-3 mb-4 mb-md-0" :key="'bew' . time()">
+            <div class="card-header border-gray-300 p-3 mb-4 mb-md-0" :key="'bew'.time()">
                 <livewire:component.action-button :actionButtons="$actionButtons" :key="'salesrep' . time()">
-                <h3 class="h5 mb-0"><i class="fas fa-bars me-1"></i> Overview</h3>
+                    <h3 class="h5 mb-0"><i class="fas fa-bars me-1"></i> Overview</h3>
             </div>
 
             <div class="card-body">
@@ -11,13 +11,15 @@
                     <li class="list-group-item d-flex align-items-center justify-content-between px-0 border-bottom">
                         <div>
                             <h3 class="h6 mb-1">Customer Number</h3>
-                            <p class="small pe-4">{{ $customerNumber }}</p>
+                            <p class="small pe-4">{{ $customerNumber }} <span
+                                    class="badge bg-light-primary">{{ $customerName }}</span></p>
                         </div>
                     </li>
                     <li class="list-group-item d-flex align-items-center justify-content-between px-0 border-bottom">
                         <div>
                             <h3 class="h6 mb-1">Ship To</h3>
-                            <p class="small pe-4">{{  $shipTo }}</p>
+                            <p class="small pe-4">{{ $shipTo }} <span
+                                    class="badge bg-light-primary">{{ strtoupper($address) }}</span></p>
                         </div>
 
 
@@ -27,34 +29,39 @@
                     <li class="list-group-item d-flex align-items-center justify-content-between px-0 border-bottom">
                         <div>
                             <h3 class="h6 mb-1">Product Line</h3>
-                            <p class="small pe-4">{{ $prodLine }}</p>
+                            <p class="small pe-4">{{ $prodLine }} <span
+                                    class="badge bg-light-primary">{{ strtoupper($line) }}</span></p>
                         </div>
                     </li>
 
                     <li class="list-group-item d-flex align-items-center justify-content-between px-0 border-bottom">
                         <div>
                             <h3 class="h6 mb-1">Sales Rep</h3>
-                            <p class="small pe-4">{{ $salesRep }}</p>
+                            <p class="small pe-4">{{ $salesRep }} <span
+                                    class="badge bg-light-primary">{{ strtoupper($operator) }}</span></p>
                         </div>
                     </li>
 
                     <li class="list-group-item d-flex align-items-center justify-content-between px-0">
                         <div>
                             <h3 class="h6 mb-1">Created At</h3>
-                            <p class="small pe-4">{{ $this->salesRepOverride->created_at?->format(config('app.default_datetime_format')) }}</p>
+                            <p class="small pe-4">
+                                {{ $this->salesRepOverride->created_at?->format(config('app.default_datetime_format')) }}
+                            </p>
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-        <div class="col-12 col-md-4 col-xxl-4">
-            <x-tabs tabId="sales-rep-comment-tabs">
-                <x-slot:tab_content_comments component="x-comments" :entity="$salesRepOverride" :key="'comments' ">
-                </x-slot>
+    <div class="col-12 col-md-4 col-xxl-4">
+        <x-tabs tabId="sales-rep-comment-tabs">
+            <x-slot:tab_content_comments component="x-comments" :entity="$salesRepOverride" :key="'comments'">
+            </x-slot>
 
-                <x-slot:tab_content_activity component="x-activity-log" :entity="$salesRepOverride" recordType="sales-rep-override" :key="'activity-'">
-                </x-slot>
-            </x-tabs>
-        </div>
+            <x-slot:tab_content_activity component="x-activity-log" :entity="$salesRepOverride" recordType="sales-rep-override"
+                :key="'activity-'">
+            </x-slot>
+        </x-tabs>
+    </div>
 </div>
