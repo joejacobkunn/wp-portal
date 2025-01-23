b1caa58a7e04d3facd1364fac1cc3872601111b5
arunRecbrT <arunraj@recybertech.com>
2025-01-23 22:15:02 +0530

    WP-27 schedule calander date zone_id fix

--- app/Http/Livewire/Scheduler/Schedule/Index.php
+++ app/Http/Livewire/Scheduler/Schedule/Index.php
@@ -343,7 +343,7 @@ class Index extends Component
                 'spanText' => $truck->zone?->name. ' - '.$truck->truck->truck_name,
                 'whse' => $truck->truck->whse,
                 'zone' => $truck->zone?->name,
-                'zone_id' => $truck->zone?->id,
+                'zone_id' => $truck->zone_id,
                 'start_time' => $truck->start_time,
                 'end_time' => $truck->end_time,
                 'slots' => $truck->slots,
