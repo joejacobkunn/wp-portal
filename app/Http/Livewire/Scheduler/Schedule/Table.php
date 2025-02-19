d97c6463ecbb76497810087598811da7a8db7592
arunRecbrT <arunraj@recybertech.com>
2025-02-19 21:02:17 +0530

    WP-27 schedules table warehouse update

--- app/Http/Livewire/Scheduler/Schedule/Table.php
+++ app/Http/Livewire/Scheduler/Schedule/Table.php
@@ -274,10 +274,10 @@ class Table extends DataTableComponent
                     }]);
             }
         ]);
-
         if (!empty($this->whse)) {
+
             //@TODO update after schedule whse update
-            $scheduleQuery->whereIn('truck_schedules.truck_id', $this->trucks->where('whse', $this->whse)->pluck('id')->toArray());
+            $scheduleQuery->where('schedules.whse', $this->whse);
 
             //$scheduleQuery->where('orders.whse', $this->whse);
         }
